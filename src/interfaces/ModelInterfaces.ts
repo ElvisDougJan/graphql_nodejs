e3d2b8cfcba24ce8e5f3aa5f53ea0538bd0d26fd
Elvis Douglas Janegitz <edjelvis@gmail.com>
2018-08-12 11:52:57 -0300

    Implementando CommentModel

--- src/interfaces/ModelInterfaces.ts
+++ src/interfaces/ModelInterfaces.ts
@@ -1,7 +1,9 @@
 import { UserModel } from "../models/UserModel";
 import { PostModel } from "../models/PostModels";
+import { CommentModel } from "../models/CommentModel";
 
 export interface ModelsInterface {
   User: UserModel;
   Post: PostModel;
+  Comment: CommentModel;
 }
